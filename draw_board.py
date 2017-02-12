56a4154286c9f9d62d7c3728fa31d2a52db53c10
Francois Roewer-Despres <francois-rd@users.noreply.github.com>
2017-02-11 21:10:07 -0600

    Cleaned up draw_board.py a little in order to making the nomenclature between files more consistent.

--- draw_board.py
+++ draw_board.py
@@ -1,21 +1,16 @@
 from state import *
 
 """
-We've got 0-24 as guard positions and 25 as alive/dead
-and we've got 100-124 as dragon positions 125 as alive/dead.
-unit_array is an array of bytes where unit_array[0] is the king.
-The guard will come in the next three places and the final 5 
-indices will be the dragons.
+This is how the board should be drawn.
 
-5   .  .  K  .  . 
+5   .  .  K  .  .
 4   .  G  G  G  .
-3   .  .  .  .  . 
+3   .  .  .  .  .
 2   D  D  D  D  D
 1   .  .  .  .  .
 
     A  B  C  D  E
 
-This is how the board should be drawn.
 Internally, the board is represented by converting the state (as a byte array),
 to a list of 5 lists of strings, as such:
 
@@ -26,43 +21,35 @@ to a list of 5 lists of strings, as such:
 
 """
 
-_SPACES = ' '
-_CHARS = ['A', 'B', 'C', 'D', 'E']
-_NUMBERS = ['5', '4', '3', '2', '1']
-_BLANK = ' '
+_SPACE = ' '
+_FILE = ['A', 'B', 'C', 'D', 'E']
+_RANK = ['5', '4', '3', '2', '1']
 
 
-def get_pos(pos):
+def get_pos(tile_idx):
     """
-    Convert an integer 0-24 representing a place on the board into an integer 
-    which corresponds to the same location on a board with a "list of list"
-    representation.
-    :return: The offsets of the row and columns of the board location.
-    :rtype: (Int, Int)
+    Converts a tile index into (row, col) pair, which are the indices into the
+    list of list of string representation of the board corresponding to the
+    same position.
+
+    :param tile_idx: position of the piece as in number from 0 to 24
+    :type tile_idx: byte
+    :return: the offsets of the row and columns of the board location
+    :rtype: (int, int)
     """
-    pos %= DRAGON_BASE
-    if pos == DEAD:
-        return None
-    col = pos // BOARD_NUM_RANKS
-    pos %= BOARD_NUM_RANKS
-    row = abs(pos - 4)
-    return row, col
+    return abs(tile_idx % BOARD_NUM_RANKS - 4), tile_idx // BOARD_NUM_RANKS
 
 
 def draw_board(state):
     """
-    Draw the board from a state.
-    :param state: A game state encoded in an array of 9 bytes.
-    :type state: State
-    """
-    board = []
-    for row in range(BOARD_NUM_RANKS):
-        board.append([])
-        for _ in range(BOARD_NUM_FILES):
-            board[row].append(_BLANK)
+    Draws the board from the given state representation to standard output.
 
-    king_pos = get_king_tile_index(state)
-    row, col = get_pos(king_pos)
+    :param state: a compact state representation
+    :type state: array of bytes
+    """
+    board = [[EMPTY] * BOARD_NUM_FILES for _ in range(BOARD_NUM_RANKS)]
+
+    row, col = get_pos(get_king_tile_index(state))
     board[row][col] = KING
 
     for _, guard_idx in get_live_guards_enumeration(state):
@@ -73,19 +60,18 @@ def draw_board(state):
         row, col = get_pos(dragon_idx - DRAGON_BASE)
         board[row][col] = DRAGON
 
-    for i in range(len(board)):
-        print(_NUMBERS[i] + _SPACES, end='')
-        print(_SPACES + board[i][0] + _SPACES +
-              _SPACES + board[i][1] + _SPACES +
-              _SPACES + board[i][2] + _SPACES +
-              _SPACES + board[i][3] + _SPACES +
-              _SPACES + board[i][4] + _SPACES)
-    # print('')
+    for i, rank in enumerate(board):
+        print(_RANK[i] + _SPACE, end='')
+        for tile_content in rank:
+            print(_SPACE + tile_content, end='')
+        print('')
+    print('')
 
-    print(' ' + _SPACES, end='')
-    for c in _CHARS:
-        print(_SPACES + c + _SPACES, end='')
+    print(_SPACE + _SPACE, end='')
+    for f in _FILE:
+        print(_SPACE + f, end='')
     print('')
 
 
-draw_board(DEFAULT_INITIAL_STATE)
+if __name__ == "__main__":
+    draw_board(DEFAULT_INITIAL_STATE)
