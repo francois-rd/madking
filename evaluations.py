f7842d55cd68136adcab9aeb8628f396c0bc6fcd
Rowan MacLachlan (rdm695) <rdm695@mail.usask.ca>
2017-02-17 11:41:34 -0600

    Adjusted simple evaluation algorithm to return a positive value for states good for King team, and negative values in states good for Dragon team.  The algorithm returns zero at initial game state

--- evaluations.py
+++ evaluations.py
@@ -3,26 +3,25 @@ from state import *
 
 def simple_eval(state, expanded_state):
     """
-    Returns <number-of-guards> - <number-of-dragons> if it's the king player's
-    turn, and <number-of-dragons> - <number-of-guards> if it's the dragon
-    player's turn.
+    Get the utility value of the state according to the rule
+    '' '' 
+    This value should be positive when the board is in favour of 
+    the King's team, and negative when it is in favour of the Dragon's
+    team.
 
     :param state: the current node in the search
     :type state: array of bytes
     :param expanded_state: to match function signature; ignored in this
         implementation
-    :return: <number-of-guards> - <number-of-dragons> if it's the king player's
-        turn, and <number-of-dragons> - <number-of-guards> if it's the dragon
-        player's turn
+    :return: 
     :rtype: int
     """
-    dragons = len(get_live_dragon_enumeration(state))
-    guards = len(get_live_guards_enumeration(state))
-    # TODO: so... the utility of the default initial state is negative for the
-    #       king and positive for the dragon? This is wrong. The evaluation
-    #       function should evaluate how good the board position is from player
-    #       1's (the king's) point of view, regardless of who's turn it is.
-    if player_turn(state) == KING_PLAYER:
-        return guards - dragons
-    else:
-        return dragons - guards
+    king_value = 1
+    num_dragons = len(get_live_dragon_enumeration(state))
+    num_guards = len(get_live_guards_enumeration(state))
+    guard_weight = (5/4)
+    dragon_weight = 1
+    dragon_score = num_dragons * dragon_weight
+    guard_score = round(num_guards * guard_weight) + king_value
+    return guard_score - dragon_score
+
