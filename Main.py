a5ec7c287e7cefa81ac4ee3a5f3835f1ace7908c
Francois Roewer-Despres <francois-rd@users.noreply.github.com>
2017-02-19 01:52:10 -0600

    Added interactive AI vs. AI mode (as a step towards non-interactive mode).

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 from ui import *
 from minimax import *
+from time import sleep
 from evaluations import simple_eval
 
 
@@ -43,7 +44,6 @@ def next_move(state, expanded_state, from_human, age):
         return get_player_move(state, expanded_state)
     else:
         from threading import Thread
-        from time import sleep
 
         class MyThread(Thread):
             def __init__(self):
@@ -73,7 +73,7 @@ def next_move(state, expanded_state, from_human, age):
         return move
 
 
-def play_ply(state, expanded_state, for_human, move_number, age):
+def play_ply(state, expanded_state, for_human, pause_for, move_number, age):
     """
     Executes a full game ply either for a human player or for the AI. Returns
     True iff the game has reached a terminal state as a result of the ply.
@@ -84,6 +84,9 @@ def play_ply(state, expanded_state, for_human, move_number, age):
     :type expanded_state: dict(byte, char)
     :param for_human: is this ply played by a human?
     :type for_human: bool
+    :param pause_for: number of seconds to pause for after getting the move
+        from the human player or the AI before actually making the move
+    :type pause_for: int or float
     :param move_number: which move is this in the game?
     :type move_number: int
     :param age: an age counter, which increases (outside of calls to play_ply)
@@ -95,6 +98,7 @@ def play_ply(state, expanded_state, for_human, move_number, age):
     draw_board(state, move_number)
     from_tile_idx, to_tile_idx = \
         next_move(state, expanded_state, for_human, age)
+    sleep(pause_for)
     move_piece(state, expanded_state, from_tile_idx, to_tile_idx)
     print('')
     print('')
@@ -105,7 +109,7 @@ def play_ply(state, expanded_state, for_human, move_number, age):
     return terminal
 
 
-def play(player_2_is_human, player_1_is_human):
+def play(player_2_is_human, player_1_is_human, pause_for=0):
     """
     A game loop that has either a human player or the AI playing interactively
     against another human or the AI itself. Recall that player 2 plays first!
@@ -114,20 +118,26 @@ def play(player_2_is_human, player_1_is_human):
     :type player_2_is_human: bool
     :param player_1_is_human: True iff the a human is controlling player 1
     :type player_1_is_human: bool
+    :param pause_for: number of seconds to pause for after getting the move
+        from the human player or the AI before actually making the move;
+        defaults to 0
+    :type pause_for: int or float
     """
     age = 0
-    move_num, state, expanded_state = setup_game()
+    move_number, state, expanded_state = setup_game()
     while True:
         num_guards = len(get_live_guards_enumeration(state))
         num_dragons = len(get_live_dragon_enumeration(state))
-        if play_ply(state, expanded_state, player_2_is_human, move_num, age):
+        if play_ply(state, expanded_state, player_2_is_human, pause_for,
+                    move_number, age):
             break
-        if play_ply(state, expanded_state, player_1_is_human, move_num, age):
+        if play_ply(state, expanded_state, player_1_is_human, pause_for,
+                    move_number, age):
             break
         if num_guards != len(get_live_guards_enumeration(state)) or \
                 num_dragons != len(get_live_dragon_enumeration(state)):
             age += 1
-        move_num += 1
+        move_number += 1
 
 
 def play_single_player():
@@ -151,7 +161,27 @@ def play_two_player():
     """
     A game loop that has a two human players playing against each other.
     """
+    print('')
     play(True, True)
+
+
+def play_ai():
+    """
+    A game loop that has the AI playing against itself, and displaying the
+    game to onlookers.
+    """
+    while True:
+        pause_for = input("How long to pause between moves? [non-negative] ")
+        try:
+            pause_for = float(pause_for)
+            if pause_for < 0:
+                print("Invalid duration:", pause_for)
+            else:
+                break
+        except ValueError:
+            print("Invalid duration: '" + pause_for + "'")
+    print('')
+    play(False, False, pause_for)
 
 
 if __name__ == "__main__":
@@ -159,11 +189,13 @@ if __name__ == "__main__":
     print("We hope you have fun playing 'The Mad King!' game!")
     print('')
     while True:
-        mode = input("Single or Two Player Mode? [s/t] ")
-        if mode in ["s", "t"]:
+        mode = input("Single Player, Two Player, or AI Mode? [s/t/a] ")
+        if mode in ["s", "t", "a"]:
             break
         print("Invalid choice: '" + mode + "'")
     if mode == "s":
         play_single_player()
-    else:
+    elif mode == "t":
         play_two_player()
+    else:
+        play_ai()
