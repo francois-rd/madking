a05dce7811891ba8822911e17345c1b406c3addd
Francois Roewer-Despres <francois-rd@users.noreply.github.com>
2017-02-20 16:32:52 -0600

    Added move time recording, and some refactoring for it.

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ from ui import *
 from minimax import *
 from time import sleep
 from evaluations import simple_eval
+from utils import record_move_search_time
+from search import iterative_deepening_search
 
 
 def setup_game():
@@ -23,7 +25,8 @@ def setup_game():
     return move_number, state, expanded_state
 
 
-def next_move(state, expanded_state, from_human, age):
+def next_move(state, expanded_state, from_human, evaluate, search, age,
+              max_depth):
     """
     Returns the next move to be played for this ply, either from a human or
     else from the AI.
@@ -34,9 +37,23 @@ def next_move(state, expanded_state, from_human, age):
     :type expanded_state: dict(byte, char)
     :param from_human: should we get the next move from a human?
     :type from_human: bool
+    :param evaluate: a function taking a state and an expanded state and
+        returning a heuristic estimate of the state's utility for the current
+        player
+    :type evaluate: (array of bytes, dict(byte, char)) => numeric
+    :param search: a search function taking a state, an expanded state, an
+        evaluation function, an age, a remaining depth, and returning a
+        ((<utility>, <exact>), <move>) pair
+    :type search: (array of bytes,
+                   dict(byte, char),
+                   (array of bytes, dict(byte, char)) => numeric,
+                   int,
+                   int) => ((numeric, bool), (byte, byte))
     :param age: an age counter, which increases (outside of calls to next_move)
         whenever a move is made that captures a piece
     :type age: int
+    :param max_depth: the maximum search depth; must be at least 1
+    :type max_depth: int
     :return: a valid move as a (<from-tile-index>, <to-tile-index>) pair
     :rtype: (byte, byte)
     """
@@ -51,9 +68,13 @@ def next_move(state, expanded_state, from_human, age):
                 self._results = None
 
             def run(self):
-                self._results = \
-                    minimax(state, expanded_state, simple_eval, age,
-                            DEFAULT_DEPTH_LIMIT, DEFAULT_DEPTH_LIMIT)
+                from time import clock
+                start_t = clock()
+                results = iterative_deepening_search(state, expanded_state,
+                                                     evaluate, search, age,
+                                                     max_depth)
+                runtime = clock() - start_t
+                self._results = (results, runtime)
 
             def result(self):
                 return self._results
@@ -66,14 +87,16 @@ def next_move(state, expanded_state, from_human, age):
             print(".", end='', flush=True)
         print('', flush=True)
         thread.join()
-        (utility, exact), move = thread.result()
-        print("The move is:", string_position(move[0]) +
-              string_position(move[1]))
+        ((utility, exact), move), time = thread.result()
+        print("After", time, "seconds, the chosen move is:",
+              string_position(move[0]) + string_position(move[1]))
         print("Move utility:", utility, "(exact)" if exact else "(estimated)")
+        record_move_search_time(time, evaluate, search, max_depth)
         return move
 
 
-def play_ply(state, expanded_state, for_human, pause_for, move_number, age):
+def play_ply(state, expanded_state, for_human, pause_for, move_number,
+             evaluate, search, age, max_depth):
     """
     Executes a full game ply either for a human player or for the AI. Returns
     True iff the game has reached a terminal state as a result of the ply.
@@ -89,15 +112,29 @@ def play_ply(state, expanded_state, for_human, pause_for, move_number, age):
     :type pause_for: int or float
     :param move_number: which move is this in the game?
     :type move_number: int
+    :param evaluate: a function taking a state and an expanded state and
+        returning a heuristic estimate of the state's utility for the current
+        player
+    :type evaluate: (array of bytes, dict(byte, char)) => numeric
+    :param search: a search function taking a state, an expanded state, an
+        evaluation function, an age, a remaining depth, and returning a
+        ((<utility>, <exact>), <move>) pair
+    :type search: (array of bytes,
+                   dict(byte, char),
+                   (array of bytes, dict(byte, char)) => numeric,
+                   int,
+                   int) => ((numeric, bool), (byte, byte))
     :param age: an age counter, which increases (outside of calls to play_ply)
         whenever a move is made that captures a piece
     :type age: int
+    :param max_depth: the maximum search depth; must be at least 1
+    :type max_depth: int
     :return: True iff the game has reached a terminal state
     :rtype: bool
     """
     draw_board(state, move_number)
-    from_tile_idx, to_tile_idx = \
-        next_move(state, expanded_state, for_human, age)
+    from_tile_idx, to_tile_idx = next_move(state, expanded_state, for_human,
+                                           evaluate, search, age, max_depth)
     sleep(pause_for)
     move_piece(state, expanded_state, from_tile_idx, to_tile_idx)
     print('')
@@ -109,7 +146,8 @@ def play_ply(state, expanded_state, for_human, pause_for, move_number, age):
     return terminal
 
 
-def play(player_2_is_human, player_1_is_human, pause_for=0):
+def play(player_2_is_human, player_1_is_human, evaluate, search, max_depth,
+         pause_for=0):
     """
     A game loop that has either a human player or the AI playing interactively
     against another human or the AI itself. Recall that player 2 plays first!
@@ -118,6 +156,20 @@ def play(player_2_is_human, player_1_is_human, pause_for=0):
     :type player_2_is_human: bool
     :param player_1_is_human: True iff the a human is controlling player 1
     :type player_1_is_human: bool
+    :param evaluate: a function taking a state and an expanded state and
+        returning a heuristic estimate of the state's utility for the current
+        player
+    :type evaluate: (array of bytes, dict(byte, char)) => numeric
+    :param search: a search function taking a state, an expanded state, an
+        evaluation function, an age, a remaining depth, and returning a
+        ((<utility>, <exact>), <move>) pair
+    :type search: (array of bytes,
+                   dict(byte, char),
+                   (array of bytes, dict(byte, char)) => numeric,
+                   int,
+                   int) => ((numeric, bool), (byte, byte))
+    :param max_depth: the maximum search depth; must be at least 1
+    :type max_depth: int
     :param pause_for: number of seconds to pause for after getting the move
         from the human player or the AI before actually making the move;
         defaults to 0
@@ -129,10 +181,10 @@ def play(player_2_is_human, player_1_is_human, pause_for=0):
         num_guards = len(get_live_guards_enumeration(state))
         num_dragons = len(get_live_dragon_enumeration(state))
         if play_ply(state, expanded_state, player_2_is_human, pause_for,
-                    move_number, age):
+                    move_number, evaluate, search, age, max_depth):
             break
         if play_ply(state, expanded_state, player_1_is_human, pause_for,
-                    move_number, age):
+                    move_number, evaluate, search, age, max_depth):
             break
         if num_guards != len(get_live_guards_enumeration(state)) or \
                 num_dragons != len(get_live_dragon_enumeration(state)):
@@ -140,9 +192,24 @@ def play(player_2_is_human, player_1_is_human, pause_for=0):
         move_number += 1
 
 
-def play_single_player():
+def play_single_player(evaluate, search, max_depth):
     """
     A game loop that has a human player playing interactively against the AI.
+
+    :param evaluate: a function taking a state and an expanded state and
+        returning a heuristic estimate of the state's utility for the current
+        player
+    :type evaluate: (array of bytes, dict(byte, char)) => numeric
+    :param search: a search function taking a state, an expanded state, an
+        evaluation function, an age, a remaining depth, and returning a
+        ((<utility>, <exact>), <move>) pair
+    :type search: (array of bytes,
+                   dict(byte, char),
+                   (array of bytes, dict(byte, char)) => numeric,
+                   int,
+                   int) => ((numeric, bool), (byte, byte))
+    :param max_depth: the maximum search depth; must be at least 1
+    :type max_depth: int
     """
     while True:
         player_choice = input("King or Dragon Player? [k/d] ")
@@ -154,21 +221,52 @@ def play_single_player():
         print("You're the Dragon Player, so you play first!")
     else:
         print("You're the King Player, so you play second!")
-    play(player_choice == "d", player_choice != "d")
+    play(player_choice == "d", player_choice != "d", evaluate, search,
+         max_depth)
 
 
-def play_two_player():
+def play_two_player(evaluate, search, max_depth):
     """
     A game loop that has a two human players playing against each other.
+
+    :param evaluate: a function taking a state and an expanded state and
+        returning a heuristic estimate of the state's utility for the current
+        player
+    :type evaluate: (array of bytes, dict(byte, char)) => numeric
+    :param search: a search function taking a state, an expanded state, an
+        evaluation function, an age, a remaining depth, and returning a
+        ((<utility>, <exact>), <move>) pair
+    :type search: (array of bytes,
+                   dict(byte, char),
+                   (array of bytes, dict(byte, char)) => numeric,
+                   int,
+                   int) => ((numeric, bool), (byte, byte))
+    :param max_depth: the maximum search depth; must be at least 1
+    :type max_depth: int
     """
     print('')
-    play(True, True)
+    play(True, True, evaluate, search, max_depth)
 
 
-def play_ai_only():
+def play_ai_only(evaluate, search, max_depth):
     """
     A game loop that has the AI playing against itself, and displaying the
     game to onlookers.
+
+    :param evaluate: a function taking a state and an expanded state and
+        returning a heuristic estimate of the state's utility for the current
+        player
+    :type evaluate: (array of bytes, dict(byte, char)) => numeric
+    :param search: a search function taking a state, an expanded state, an
+        evaluation function, an age, a remaining depth, and returning a
+        ((<utility>, <exact>), <move>) pair
+    :type search: (array of bytes,
+                   dict(byte, char),
+                   (array of bytes, dict(byte, char)) => numeric,
+                   int,
+                   int) => ((numeric, bool), (byte, byte))
+    :param max_depth: the maximum search depth; must be at least 1
+    :type max_depth: int
     """
     while True:
         pause_for = input("How long to pause between moves? [non-negative] ")
@@ -181,7 +279,7 @@ def play_ai_only():
         except ValueError:
             print("Invalid duration: '" + pause_for + "'")
     print('')
-    play(False, False, pause_for)
+    play(False, False, evaluate, search, max_depth, pause_for)
 
 
 if __name__ == "__main__":
@@ -194,8 +292,8 @@ if __name__ == "__main__":
             break
         print("Invalid choice: '" + mode + "'")
     if mode == "s":
-        play_single_player()
+        play_single_player(simple_eval, minimax, DEFAULT_DEPTH_LIMIT)
     elif mode == "t":
-        play_two_player()
+        play_two_player(simple_eval, minimax, DEFAULT_DEPTH_LIMIT)
     else:
-        play_ai_only()
+        play_ai_only(simple_eval, minimax, DEFAULT_DEPTH_LIMIT)
--- utils.py
+++ utils.py
@@ -22,3 +22,31 @@ def parse_move(str_move):
             str_move[2] not in FILE or str_move[3] not in RANK:
         return False, -1, -1
     return True, tile_index(str_move[0:2]), tile_index(str_move[2:4])
+
+
+def record_move_search_time(time, evaluate, search, max_depth):
+    """
+    Records the given time to the "timing.csv" file, along with the
+    corresponding evaluation function, search algorithm, and maximum search
+    depth.
+
+    :param time: the time taken for the search
+    :type time: float
+    :param evaluate: a function taking a state and an expanded state and
+        returning a heuristic estimate of the state's utility for the current
+        player
+    :type evaluate: (array of bytes, dict(byte, char)) => numeric
+    :param search: a search function taking a state, an expanded state, an
+        evaluation function, an age, a remaining depth, and returning a
+        ((<utility>, <exact>), <move>) pair
+    :type search: (array of bytes,
+                   dict(byte, char),
+                   (array of bytes, dict(byte, char)) => numeric,
+                   int,
+                   int) => ((numeric, bool), (byte, byte))
+    :param max_depth: the maximum search depth; must be at least 1
+    :type max_depth: int
+    """
+    with open("timing.csv", "w") as file:
+        file.write(','.join(str(i) for i in [time, evaluate.__name__,
+                                             search.__name__, max_depth]))
