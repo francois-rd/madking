7196392c661c5bf4a7116161bce6dfa403e9a2e9
Rowan MacLachlan (rdm695) <rdm695@mail.usask.ca>
2017-02-19 18:04:26 -0600

    Added one method to evaluation to determine openness of kings placement

--- evaluations.py
+++ evaluations.py
@@ -23,3 +23,48 @@ def simple_eval(state, expanded_state):
     dragon_score = num_dragons * dragon_weight
     guard_score = round(num_guards * guard_weight) + king_value
     return guard_score - dragon_score
+
+
+def get_tiles_around(pos):
+    """
+    Get the numbers' of tiles around the board 
+    position 'pos', orthogonally.
+    
+    :param pos: the board position of interest, 0-24
+    :type pos: int
+    :return: The number of tiles around the position 'pos'.
+    :rtype: [int]
+    """
+    tiles = []
+    left = pos - BOARD_NUM_RANKS
+    right = pos + BOARD_NUM_RANKS
+    above = pos + 1
+    below = pos - 1
+    size = BOARD_NUM_RANKS * BOARD_NUM_FILES
+    if left < size and left >= 0:
+        tiles.append(left)
+    if right < size and right >= 0:
+        tiles.append(right)
+    if above % BOARD_NUM_RANKS != 0:
+        tiles.append(above)
+    if below % BOARD_NUM_RANKS != 0:
+        tiles.append(below)
+    return tiles
+
+
+def is_king_at_risk(king_pos, expanded_state):
+    """
+    Return true or false depending on whether or not the king
+    is in an orientation which is safe from check or not.
+    
+    :param king_pos: A number 0-24 indicating the placement of the
+        king on the board:
+    :type king_pos: int
+    :param expanded_state: 
+    :type expanded_state: dict(int, char)
+    :return: True if there are three open spots around the king, 
+        and false otherwise.
+    :rtype: Boolean
+    """
+    surrounding_tiles = get_tiles_around(king_pos)
+
